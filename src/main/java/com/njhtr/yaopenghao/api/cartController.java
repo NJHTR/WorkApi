6e141c7687d1666802f370a78cf53802c0952aae
NJHTRY <njhtr1018@outlook.com>
2025-06-19 17:31:37 +0800

    feat: 新增cart列表接口

--- src/main/java/com/njhtr/yaopenghao/api/cartController.java
+++ src/main/java/com/njhtr/yaopenghao/api/cartController.java
@@ -1,10 +1,15 @@
 package com.njhtr.yaopenghao.api;
 
+import com.njhtr.yaopenghao.entity.response.Result;
 import com.njhtr.yaopenghao.service.cartService;
+import com.njhtr.yaopenghao.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/cart")
@@ -13,4 +18,11 @@ public class cartController {
     @Autowired
     private cartService cartService;
 
+    @GetMapping("")
+    public Result userCart(@RequestHeader("Authorization") String token){
+        Map<String, Object> claims = JWTUtil.ParseToken(token);
+        String userId = claims.get("id").toString();
+
+        return Result.success();
+    }
 }
